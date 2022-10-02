014703850806c2c7eef94406856ec501299bbf29
Daniil <danielfalcony@gmail.com>
2022-10-02 18:00:25 +0300

    1 task done 2 task done 3 task done 4 task not done

---
+++ src/HW_1/task4.java
@@ -0,0 +1,14 @@
+// Вычислить n-ое треугольное число
+
+package HW_1;
+
+import java.util.Arrays;
+
+public class task4 {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+// Вообще не понятна задача и как к ней подступиться, до этого с такими не сталкивался.
