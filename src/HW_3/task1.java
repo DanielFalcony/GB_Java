2df0c34562f54f8432ddd0b3b396ac1823d514b1
Daniil <danielfalcony@gmail.com>
2022-10-09 18:25:10 +0300

    1 task done 2 task done

---
+++ src/HW_3/task1.java
@@ -0,0 +1,19 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+package HW_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class task1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) list.remove(i);
+        }
+        System.out.println(list);
+    }
+}
