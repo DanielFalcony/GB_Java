57d183412481f78db918df98c7fe79800bb63afd
Daniil <danielfalcony@gmail.com>
2022-10-16 20:21:42 +0300

    task1 Done task2 Done task3 Done

--- src/HW_4/task3.java
+++ src/HW_4/task3.java
@@ -2,5 +2,21 @@
 
 package HW_4;
 
+import java.util.LinkedList;
+
 public class task3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        int sum = 0;
+        for (int i = 0; i < 31; i++) {
+            list.add(i);
+        }
+
+        for (Integer integer : list) {
+            sum += integer;
+        }
+
+        System.out.println(list);
+        System.out.println(sum);
+    }
 }
