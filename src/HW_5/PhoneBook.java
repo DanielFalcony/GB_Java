27d1e85b165d3b24ddcf675a44561afe78cf6f27
DanielFalcony <DanielFalcony@gmail.com>
2022-10-24 17:06:01 +0300

    Crash mind

--- src/HW_5/PhoneBook.java
+++ src/HW_5/PhoneBook.java
@@ -10,13 +10,26 @@ public class PhoneBook {
         map = new HashMap<>();
     }
 
-    public void add(String number, String name){
-        map.put(number, name);
+    public void add(String name, String number){
+        if (map.size() == 0){
+            map.put(name, number);
+        }
+        else {
+            for (String key : map.keySet()) {
+                if (name.equals(key)){
+                    map.keySet().add(number);
+                }
+                else {
+                    map.put(name, number);
+                }
+            }
+        }
     }
 
     public void printPerson() {
         for (String key : map.keySet()) {
             System.out.println(key + " " + map.get(key));
+
         }
     }
 }
--- src/HW_5/task1.java
+++ src/HW_5/task1.java
@@ -5,9 +5,9 @@ package HW_5;
 public class task1 {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("89258888875", "Иванов Иван Иванович");
-        phoneBook.add("89258888875", "Петров Петр Петрович");
-        phoneBook.add("89258288875", "Смирнов Дмитрий Дмитриевич");
+        phoneBook.add("Иванов Иван Иванович", "89258888875");
+        phoneBook.add("Иванов Иван Иванович", "89253338875");
+        phoneBook.add("Смирнов Дмитрий Дмитриевич", "89258288875");
         phoneBook.printPerson();
     }
 }
