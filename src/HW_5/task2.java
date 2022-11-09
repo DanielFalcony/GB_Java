265e4cc4ddf1e13d9092a5b244bb730944b2013c
DanielFalcony <DanielFalcony@gmail.com>
2022-11-09 09:36:45 +0300

    5 in looks

--- src/HW_5/task2.java
+++ src/HW_5/task2.java
@@ -6,21 +6,26 @@
 
 package HW_5;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class task2 {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        ArrayList<String> allUsers = new ArrayList<>(Arrays.asList("Иван Иванов", "Светлана Петрова", "Кристина Белова",
-                "Анна Мусина", "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов",
-                "Мария Федорова", "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова",
-                "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов"));
-        for (int i = 0; i < allUsers.size(); i++) {
-            String result = allUsers.get(i);
-            System.out.println(result);
+        String[] names = new String[]{"Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, " +
+                "Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина," +
+                "Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов"};
+
+        var map = new myMap();
+
+        for (String name : names){
+            map.add(name);
         }
+
+        System.out.println(map);
+
+        var ST = new sortTree();
+
+        ST.sort(map.map);
+        System.out.println(ST);
 
     }
 }
